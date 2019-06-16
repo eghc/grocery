324c02a44e8cc7b03870935cd642e73e640ce354
eghc <eringraceharris@gmail.com>
2019-06-16 00:39:35 -0700

    initialization

---
+++ client/build/precache-manifest.494dc4597bb191a3f5c233d72435246f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be654385ceb36e8a698601a8bdbe3e7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfc5fe116ab9c51cc71b",
+    "url": "/static/css/main.0fdf491b.chunk.css"
+  },
+  {
+    "revision": "3029ebb9601011892207",
+    "url": "/static/js/2.f5ee53cd.chunk.js"
+  },
+  {
+    "revision": "bfc5fe116ab9c51cc71b",
+    "url": "/static/js/main.52896a29.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
